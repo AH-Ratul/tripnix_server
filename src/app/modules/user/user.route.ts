c027ff5d2ef74d4f1c57a7c2f1d5c153186fe987
AH-Ratul <ahratul21@gmail.com>
2025-07-13 20:34:35 +0600

    validate route via zod

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 export const userRouter = Router();
 
-userRouter.post("/register", userController.createUser);
+userRouter.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  userController.createUser
+);
 userRouter.get("/all-users", userController.gettAllUsers);
