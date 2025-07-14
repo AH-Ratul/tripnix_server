40195767993099c929e85a105ab8825514a3a8f9
AH-Ratul <ahratul21@gmail.com>
2025-07-15 00:46:57 +0600

    feat: handle controller to user update requests

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,9 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
-import AppError from "../../errorHelpers/AppError";
 import { catchAsync } from "../../utils/catchAsync";
-import { success } from "zod";
 import { sendResponse } from "../../utils/sendResponse";
 
 const createUser = catchAsync(
@@ -11,10 +9,31 @@ const createUser = catchAsync(
     const user = await UserServices.createUser(req.body);
 
     sendResponse(res, {
-      statusCode: httpStatus.OK,
+      statusCode: httpStatus.CREATED,
       success: true,
-      message: "User created successfully",
+      message: "User Created Successfully",
       data: user,
+    });
+  }
+);
+
+const updateUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
+    const verifiedToken = req.user;
+    const payload = req.body;
+
+    const updatedUser = await UserServices.updateUser(
+      userId,
+      payload,
+      verifiedToken
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
+      success: true,
+      message: "User Updated Successfully",
+      data: updatedUser,
     });
   }
 );
@@ -26,7 +45,7 @@ const gettAllUsers = catchAsync(
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: "User retrieved successfully",
+      message: "User Retrieved Successfully",
       meta: result.meta,
       data: result.users,
     });
@@ -36,4 +55,5 @@ const gettAllUsers = catchAsync(
 export const userController = {
   createUser,
   gettAllUsers,
+  updateUser,
 };
