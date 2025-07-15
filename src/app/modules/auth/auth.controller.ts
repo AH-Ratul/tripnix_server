b08df1f58ebb2c56baf77ebe13d43dbb94b69f71
AH-Ratul <ahratul21@gmail.com>
2025-07-15 18:44:11 +0600

    feat: manage refresh token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -17,6 +17,22 @@ const credentialsLogin = catchAsync(
   }
 );
 
+const getNewAccessToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "User Login Successfull",
+      data: tokenInfo,
+    });
+  }
+);
+
 export const AuthController = {
   credentialsLogin,
+  getNewAccessToken
 };
