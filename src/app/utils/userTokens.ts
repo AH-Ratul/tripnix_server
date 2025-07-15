eb3da7014846a46578d0bf9b5a39da7ca604dee9
AH-Ratul <ahratul21@gmail.com>
2025-07-15 20:24:01 +0600

    feat: manage new accessToken with refreshToken

--- src/app/utils/userTokens.ts
+++ src/app/utils/userTokens.ts
@@ -1,6 +1,10 @@
+import { JwtPayload } from "jsonwebtoken";
 import { config } from "../config";
-import { IUser } from "../modules/user/user.interface";
-import { generateToken } from "./jwt";
+import { IsActive, IUser } from "../modules/user/user.interface";
+import { User } from "../modules/user/user.model";
+import { generateToken, verifyToken } from "./jwt";
+import httpStatus from "http-status-codes";
+import AppError from "../errorHelpers/AppError";
 
 export const createUserTokens = (user: Partial<IUser>) => {
   const jwtPaylaod = {
@@ -25,4 +29,47 @@ export const createUserTokens = (user: Partial<IUser>) => {
     accessToken,
     refreshToken,
   };
+};
+
+export const createNewAccessTokenWithRefreshToken = async (
+  refreshToken: string
+) => {
+  const verifyRefreshToken = verifyToken(
+    refreshToken,
+    config.JWT_REFRESH_SECRET
+  ) as JwtPayload;
+
+  const isUserExist = await User.findOne({ email: verifyRefreshToken.email });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User does not Exist");
+  }
+
+  if (
+    isUserExist.isActive === IsActive.BLOCKED ||
+    isUserExist.isActive === IsActive.INACTIVE
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `User is ${isUserExist.isActive}`
+    );
+  }
+
+  if (isUserExist.isDeleted === true) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is Deleted");
+  }
+
+  const jwtPaylaod = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role,
+  };
+
+  const accessToken = generateToken(
+    jwtPaylaod as unknown as string,
+    config.JWT_SECRET,
+    config.JWT_EXPIRES
+  );
+
+  return accessToken;
 };
