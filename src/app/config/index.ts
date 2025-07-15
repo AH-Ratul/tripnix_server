8b16068e5fa647c32bc3e4c4f58fbf708d8c9097
AH-Ratul <ahratul21@gmail.com>
2025-07-15 18:39:58 +0600

    update: jwt environments

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -11,6 +11,8 @@ interface EnvConfig {
   SALT: string;
   SUPER_ADMIN_EMAIL: string;
   SUPER_ADMIN_PASSWORD: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_REFRESH_EXPIRES: string;
 }
 
 const loadEnvironments = (): EnvConfig => {
@@ -23,6 +25,8 @@ const loadEnvironments = (): EnvConfig => {
     "SALT",
     "SUPER_ADMIN_EMAIL",
     "SUPER_ADMIN_PASSWORD",
+    "JWT_REFRESH_SECRET",
+    "JWT_REFRESH_EXPIRES",
   ];
 
   requireEnvVariables.forEach((key) => {
@@ -40,6 +44,8 @@ const loadEnvironments = (): EnvConfig => {
     SALT: process.env.SALT as string,
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
   };
 };
 
