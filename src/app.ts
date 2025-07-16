e863833083a9e43bd4bdca58285891f75c345c74
AH-Ratul <ahratul21@gmail.com>
2025-07-16 23:13:31 +0600

    feat: initialized passport to app

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,22 @@ import { appRouter } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
+import "./app/config/passport";
+import { config } from "./app/config";
 
 const app = express();
 
+app.use(
+  expressSession({
+    secret: config.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
