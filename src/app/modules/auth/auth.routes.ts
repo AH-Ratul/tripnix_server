7ad999b38a413612d25e662347d02da01a7784d2
AH-Ratul <ahratul21@gmail.com>
2025-07-16 19:14:09 +0600

    feat: add reset password route with auth middleware

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 
 export const authRouter = Router();
 
 authRouter.post("/login", AuthController.credentialsLogin);
 authRouter.post("/refresh-token", AuthController.getNewAccessToken);
 authRouter.post("/logout", AuthController.logout);
+authRouter.post(
+  "/reset-password",
+  checkAuth(...Object.values(Role)),
+  AuthController.resetPassword
+);
