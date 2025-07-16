1f96e02d8fe93e7890a039905d5001a2502f5c3d
AH-Ratul <ahratul21@gmail.com>
2025-07-16 19:15:10 +0600

    feat: manage controller to reset password

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import httpStatus from "http-status-codes";
 import { AuthServices } from "./auth.services";
 import AppError from "../../errorHelpers/AppError";
 import { setAuthCookie } from "../../utils/setAuthCookie";
+import { JwtPayload } from "jsonwebtoken";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -68,8 +69,26 @@ const logout = catchAsync(
   }
 );
 
+const resetPassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user;
+    
+    await AuthServices.resetPassword(newPassword, oldPassword, decodedToken as JwtPayload);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Password Changed Successfully",
+      data: null,
+    });
+  }
+);
+
 export const AuthController = {
   credentialsLogin,
   getNewAccessToken,
   logout,
+  resetPassword,
 };
