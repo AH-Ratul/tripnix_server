8f4574a857de67587392f4d148154159d97bd0bb
AH-Ratul <ahratul21@gmail.com>
2025-07-21 13:00:56 +0600

    removed: unused login services & imports

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,43 +1,10 @@
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcrypt from "bcryptjs";
-import {
-  createNewAccessTokenWithRefreshToken,
-  createUserTokens,
-} from "../../utils/userTokens";
+import { createNewAccessTokenWithRefreshToken } from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { config } from "../../config";
-
-const credentialsLogin = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
-
-  const isUserExist = await User.findOne({ email });
-
-  if (!isUserExist) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Email Doesn't Exist");
-  }
-
-  const isPasswordMatched = await bcrypt.compare(
-    password as string,
-    isUserExist.password as string
-  );
-
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
-  }
-
-  const usertokens = createUserTokens(isUserExist);
-
-  const { password: pass, ...rest } = isUserExist.toObject();
-
-  return {
-    accessToken: usertokens.accessToken,
-    refreshToken: usertokens.refreshToken,
-    user: rest,
-  };
-};
 
 const getNewAccessToken = async (refreshToken: string) => {
   const newAccessToken = await createNewAccessTokenWithRefreshToken(
@@ -49,22 +16,31 @@ const getNewAccessToken = async (refreshToken: string) => {
   };
 };
 
-const resetPassword = async (newPassword: string, oldPassword: string, decodedToken: JwtPayload) => {
+const resetPassword = async (
+  newPassword: string,
+  oldPassword: string,
+  decodedToken: JwtPayload
+) => {
   const user = await User.findById(decodedToken.userId);
 
-  const isOldPasswordMatch = await bcrypt.compare(oldPassword, user!.password as string);
+  const isOldPasswordMatch = await bcrypt.compare(
+    oldPassword,
+    user!.password as string
+  );
 
-  if(!isOldPasswordMatch) {
-    throw new AppError(httpStatus.UNAUTHORIZED, "Old Password does not Match")
+  if (!isOldPasswordMatch) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Old Password does not Match");
   }
 
-  user!.password = await bcrypt.hash(newPassword as string, Number(config.SALT));
+  user!.password = await bcrypt.hash(
+    newPassword as string,
+    Number(config.SALT)
+  );
 
-  user!.save();  
+  user!.save();
 };
 
 export const AuthServices = {
-  credentialsLogin,
   getNewAccessToken,
-  resetPassword
+  resetPassword,
 };
