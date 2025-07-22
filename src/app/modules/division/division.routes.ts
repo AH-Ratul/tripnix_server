d89602d6b28a2556b3e3364914243a58b17a628a
AH-Ratul <ahratul21@gmail.com>
2025-07-22 21:38:55 +0600

    feat: handle division routes with authorized validation

---
+++ src/app/modules/division/division.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
+import { validateRequest } from "../../middlewares/validateRequest";
+import {
+  createDivisionSchema,
+  updateDivisionSchema,
+} from "./division.validation";
+import { DivisionController } from "./division.controller";
+
+export const divisionRouter = Router();
+
+divisionRouter.post(
+  "/create",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  validateRequest(createDivisionSchema),
+  DivisionController.createDivision
+);
+divisionRouter.get("/", DivisionController.getAllDivision);
+divisionRouter.patch(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  validateRequest(updateDivisionSchema),
+  DivisionController.updateDivision
+);
+divisionRouter.delete(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  DivisionController.deleteDivision
+);
