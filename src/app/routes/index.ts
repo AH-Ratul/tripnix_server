96a994e5a231cd85d94037e7c4edc7ac2c49a955
AH-Ratul <ahratul21@gmail.com>
2025-07-22 21:33:53 +0600

    feat: add tour & division routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userRouter } from "../modules/user/user.route";
 import { authRouter } from "../modules/auth/auth.routes";
+import { divisionRouter } from "../modules/division/division.routes";
+import { tourRouter } from "../modules/tour/tour.route";
 
 export const appRouter = Router();
 
@@ -12,6 +14,14 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: authRouter,
+  },
+  {
+    path: "/division",
+    route: divisionRouter,
+  },
+  {
+    path: "/tour",
+    route: tourRouter,
   },
 ];
 
