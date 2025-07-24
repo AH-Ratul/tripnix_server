e7129a329f87a8bd2898ced9bbf96e98c0c7892a
AH-Ratul <ahratul21@gmail.com>
2025-07-24 17:03:32 +0600

    update: timestamps

--- src/app/modules/tour/tour.model.ts
+++ src/app/modules/tour/tour.model.ts
@@ -12,55 +12,57 @@ const tourTypeSchema = new Schema<ITourType>(
 
 export const TourType = model<ITourType>("TourType", tourTypeSchema);
 
-const tourSchema = new Schema<ITour>({
-  title: {
-    type: String,
-    required: true,
+const tourSchema = new Schema<ITour>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    location: {
+      type: String,
+    },
+    costFrom: {
+      type: Number,
+    },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    departureLocation: { type: String },
+    arrivalLocation: { type: String },
+    included: { type: [String], default: [] },
+    excluded: { type: [String], default: [] },
+    amenities: { type: [String], default: [] },
+    tourPlan: { type: [String], default: [] },
+    maxGuest: { type: Number },
+    minAge: { type: Number },
+    division: {
+      type: Schema.Types.ObjectId,
+      ref: "Division",
+      required: true,
+    },
+    tourType: {
+      type: Schema.Types.ObjectId,
+      ref: "TourType",
+      required: true,
+    },
   },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  description: {
-    type: String,
-  },
-  images: {
-    type: [String],
-    default: [],
-  },
-  location: {
-    type: String,
-  },
-  costFrom: {
-    type: Number,
-  },
-  startDate: { type: Date },
-  endDate: { type: Date },
-  departureLocation: { type: String },
-  arrivalLocation: { type: String },
-  included: { type: [String], default: [] },
-  excluded: { type: [String], default: [] },
-  amenities: { type: [String], default: [] },
-  tourPlan: { type: [String], default: [] },
-  maxGuest: { type: Number },
-  minAge: { type: Number },
-  division: {
-    type: Schema.Types.ObjectId,
-    ref: "Division",
-    required: true,
-  },
-  tourType: {
-    type: Schema.Types.ObjectId,
-    ref: "TourType",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 tourSchema.pre("save", async function (next) {
   if (this.isModified("title")) {
     const baseSlug = this.title.toLowerCase().split(" ").join("-");
-    let slug = `${baseSlug}-division`;
+    let slug = `${baseSlug}`;
 
     let counter = 0;
     while (await Tour.exists({ slug })) {
@@ -78,7 +80,7 @@ tourSchema.pre("findOneAndUpdate", async function (next) {
 
   if (tour.title) {
     const baseSlug = tour.title.toLowerCase().split(" ").join("-");
-    let slug = `${baseSlug}-division`;
+    let slug = `${baseSlug}`;
 
     let counter = 0;
     while (await Tour.exists({ slug })) {
