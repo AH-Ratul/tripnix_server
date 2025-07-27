c3305d0c2ccdf3ffc34fae92762764183da82063
AH-Ratul <ahratul21@gmail.com>
2025-07-28 00:36:53 +0600

    new: environment variables for sslCommerz

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -18,6 +18,16 @@ interface EnvConfig {
   GOOGLE_CALLBACK_URL: string;
   EXPRESS_SESSION_SECRET: string;
   CLIENT_URL: string;
+  SSL_STORE_ID: string;
+  SSL_STORE_PASS: string;
+  SSL_PAYMENT_API: string;
+  SSL_VALIDATIN_API: string;
+  SSL_SUCCESS_FRONTEND_URL: string;
+  SSL_FAIL_FRONTEND_URL: string;
+  SSL_CANCEL_FRONTEND_URL: string;
+  SSL_SUCCESS_BACKEND_URL: string;
+  SSL_FAIL_BACKEND_URL: string;
+  SSL_CANCEL_BACKEND_URL: string;
 }
 
 const loadEnvironments = (): EnvConfig => {
@@ -37,6 +47,16 @@ const loadEnvironments = (): EnvConfig => {
     "GOOGLE_CALLBACK_URL",
     "EXPRESS_SESSION_SECRET",
     "CLIENT_URL",
+    "SSL_STORE_ID",
+    "SSL_STORE_PASS",
+    "SSL_PAYMENT_API",
+    "SSL_VALIDATIN_API",
+    "SSL_SUCCESS_FRONTEND_URL",
+    "SSL_FAIL_FRONTEND_URL",
+    "SSL_CANCEL_FRONTEND_URL",
+    "SSL_SUCCESS_BACKEND_URL",
+    "SSL_FAIL_BACKEND_URL",
+    "SSL_CANCEL_BACKEND_URL",
   ];
 
   requireEnvVariables.forEach((key) => {
@@ -61,6 +81,16 @@ const loadEnvironments = (): EnvConfig => {
     GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
     EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
     CLIENT_URL: process.env.CLIENT_URL as string,
+    SSL_STORE_ID: process.env.SSL_STORE_ID as string,
+    SSL_STORE_PASS: process.env.SSL_STORE_PASS as string,
+    SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+    SSL_VALIDATIN_API: process.env.SSL_VALIDATIN_API as string,
+    SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
+    SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
+    SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
+    SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
+    SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
+    SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string,
   };
 };
 
