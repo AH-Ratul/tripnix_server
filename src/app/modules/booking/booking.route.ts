e3da9fb881493f0a21937e7fb4d589edd4baf891
AH-Ratul <ahratul21@gmail.com>
2025-07-28 19:09:56 +0600

    feat: manage booking routes with athorized validation

--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -1,8 +1,43 @@
 import { Router } from "express";
 import { BookingController } from "./booking.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
+import { validateRequest } from "../../middlewares/validateRequest";
+import {
+  createBookingZodSchema,
+  updateBookingZodSchema,
+} from "./booking.validation";
 
 export const bookingRouter = Router();
 
-bookingRouter.post("/create", BookingController.createBooking);
+bookingRouter.post(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  validateRequest(createBookingZodSchema),
+  BookingController.createBooking
+);
 
-bookingRouter.get("/", BookingController.getAllBookings);
+bookingRouter.get(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  BookingController.getAllBookings
+);
+
+bookingRouter.get(
+  "/my-bookings",
+  checkAuth(...Object.values(Role)),
+  BookingController.getUserBooking
+);
+
+bookingRouter.get(
+  "/:bookingId",
+  checkAuth(...Object.values(Role)),
+  BookingController.getSingleBooking
+);
+
+bookingRouter.patch(
+  "/:bookingId/status",
+  checkAuth(...Object.values(Role)),
+  validateRequest(updateBookingZodSchema),
+  BookingController.updateBookingStatus
+);
