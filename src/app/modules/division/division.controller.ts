a9a9dd5d97def36726dd8dcaa6a1d7f2eeb930fd
AH-Ratul <ahratul21@gmail.com>
2025-08-12 11:38:33 +0600

    feat: handle controller to create division with image file

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -3,11 +3,15 @@ import { catchAsync } from "../../utils/catchAsync";
 import { DivisionService } from "./division.services";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
+import { IDivision } from "./division.interface";
 
 const createDivision = catchAsync(async (req: Request, res: Response) => {
-    console.log(req.body);
-  const result = await DivisionService.createDivision(req.body);
-  console.log("res", result)
+  const payload: IDivision = {
+    ...req.body,
+    thumbnail: req.file?.path,
+  };
+
+  const result = await DivisionService.createDivision(payload);
 
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
