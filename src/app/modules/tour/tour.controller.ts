d6a55f1ccfd75b8d411500b9e93eea4f805d3fdd
AH-Ratul <ahratul21@gmail.com>
2025-08-12 16:46:29 +0600

    feat: manage payload for image uploading

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -3,9 +3,15 @@ import { catchAsync } from "../../utils/catchAsync";
 import { TourService } from "./tour.services";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
+import { ITour } from "./tour.interface";
 
 const createTour = catchAsync(async (req: Request, res: Response) => {
-  const result = await TourService.createTour(req.body);
+  const payload: ITour = {
+    ...req.body,
+    images: (req.files as Express.Multer.File[]).map((file) => file.path),
+  };
+
+  const result = await TourService.createTour(payload);
 
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
