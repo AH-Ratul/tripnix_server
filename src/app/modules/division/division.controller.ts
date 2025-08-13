f8dd89888217669b5dfbe33e650983a886770c96
AH-Ratul <ahratul21@gmail.com>
2025-08-13 11:53:10 +0600

    feat: handle controller to update division with image file

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -47,7 +47,13 @@ const getSingleDivision = catchAsync(async (req: Request, res: Response) => {
 
 const updateDivision = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  const result = await DivisionService.updateDivision(id, req.body);
+
+  const payload: IDivision = {
+    ...req.body,
+    thumbnail: req.file?.path,
+  };
+
+  const result = await DivisionService.updateDivision(id, payload);
 
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
