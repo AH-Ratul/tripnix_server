40aa74317fabedd0260b60fe84e2d3083bf9ed49
AH-Ratul <ahratul21@gmail.com>
2025-08-13 11:58:28 +0600

    feat: handle controller to update tour with image files

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -36,7 +36,13 @@ const getAllTours = catchAsync(async (req: Request, res: Response) => {
 
 const updateTour = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  const result = await TourService.updateTour(id, req.body);
+
+  const payload: ITour = {
+    ...req.body,
+    images: (req.files as Express.Multer.File[]).map((file) => file.path),
+  };
+
+  const result = await TourService.updateTour(id, payload);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
