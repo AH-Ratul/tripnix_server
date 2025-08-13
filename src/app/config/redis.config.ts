ad9ea4a23a9c6baf3a5172abfcc0f9cbfbf9b9cf
AH-Ratul <ahratul21@gmail.com>
2025-08-13 22:56:25 +0600

    remove: unused codes

--- src/app/config/redis.config.ts
+++ src/app/config/redis.config.ts
@@ -12,10 +12,6 @@ export const redisClient = createClient({
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
-// await redisClient.set("foo", "bar");
-// const result = await redisClient.get("foo");
-// console.log(result); // >>> bar
-
 export const connectRedis = async () => {
   if (!redisClient.isOpen) {
     await redisClient.connect();
