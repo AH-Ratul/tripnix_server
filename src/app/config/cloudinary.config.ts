36c148f741be6261b7b0a4389d141b71680f1ab4
AH-Ratul <ahratul21@gmail.com>
2025-08-15 15:51:05 +0600

    feat: handle upload buffer file

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,12 +1,46 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { config } from ".";
 import AppError from "../errorHelpers/AppError";
+import stream from "stream";
 
 cloudinary.config({
   cloud_name: config.CLOUDINARY_CLOUD_NAME,
   api_key: config.CLOUDINARY_API_KEY,
   api_secret: config.CLOUDINARY_API_SECRET,
 });
+
+export const uploadBufferToCloudinary = async (
+  buffer: string,
+  fileName: string
+): Promise<UploadApiResponse | undefined> => {
+  try {
+    return new Promise((resolve, reject) => {
+      const public_id = `file/${fileName}-${Date.now()}`;
+
+      const bufferStream = new stream.PassThrough();
+      bufferStream.end(buffer);
+
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "auto",
+            public_id,
+            folder: "pdf",
+          },
+          (error, result) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+  } catch (error: any) {
+    console.log("buffer error", error);
+    throw new AppError(400, `Error Uploading file ${error.message}`);
+  }
+};
 
 export const deleteImageFromCloudinary = async (url: string) => {
   try {
