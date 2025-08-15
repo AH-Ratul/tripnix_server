d83dcd14f931ad6d09f95431d7b3e2c03fd7ff1b
AH-Ratul <ahratul21@gmail.com>
2025-08-15 23:56:10 +0600

    feat: add stats route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,6 +6,7 @@ import { tourRouter } from "../modules/tour/tour.route";
 import { bookingRouter } from "../modules/booking/booking.route";
 import { paymentRouter } from "../modules/payment/payment.route";
 import { otpRoute } from "../modules/otp/otp.route";
+import { statsRoute } from "../modules/stats/stats.route";
 
 export const appRouter = Router();
 
@@ -37,6 +38,10 @@ const moduleRoutes = [
   {
     path: "/otp",
     route: otpRoute,
+  },
+  {
+    path: "/stats",
+    route: statsRoute,
   },
 ];
 
