a8164e761830c34040c445690c2ea22beb5f75a0
AH-Ratul <ahratul21@gmail.com>
2025-08-15 16:14:16 +0600

    feat: add route to get invoice

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { PaymentController } from "./payment.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 
 export const paymentRouter = Router();
 
@@ -7,3 +9,8 @@ paymentRouter.post("/init-payment/:bookingId", PaymentController.initPayment);
 paymentRouter.post("/success", PaymentController.successPayment);
 paymentRouter.post("/fail", PaymentController.failPayment);
 paymentRouter.post("/cancel", PaymentController.cancelPayment);
+paymentRouter.get(
+  "/invoice/:paymentId",
+  checkAuth(...Object.values(Role)),
+  PaymentController.getInvoiceURL
+);
